7c50368b9112e8ff729b8c766aa36815f6c95ba7
Muntasim Ahmed <mohammad.nadeem.ahmed@autodesk.com>
2020-10-08 07:31:08 +0800

    dockerized

---
+++ upload/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/upload/catalog/');
+define('DIR_SYSTEM', '/var/www/upload/system/');
+define('DIR_IMAGE', '/var/www/upload/image/');
+define('DIR_STORAGE', '/var/www/public_html/storage/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/theme/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mpdo');
+define('DB_HOSTNAME', 'mysql');
+define('DB_USERNAME', 'opencart');
+define('DB_PASSWORD', 'password');
+define('DB_DATABASE', 'opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
